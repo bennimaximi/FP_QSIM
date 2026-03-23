f65d9bfb2762602d7ed0f52c9c699a9524dec1d0
bennimaximi <benjamingrasselt@gmail.com>
2026-03-23 16:09:16 +0100

    new pauli matrices print and check system applied

--- playground/test.py
+++ playground/test.py
@@ -1,4 +1,28 @@
 from fp_qsim import hello
+from fp_qsim.pauli import sigmax, sigmay, sigmaz
+import numpy as np
+
+# Test the hello function:
 
 output = hello()
 print(output)
+
+# Test the Pauli matrices:
+
+sx: np.ndarray = sigmax()
+sy: np.ndarray = sigmay()
+sz: np.ndarray = sigmaz()
+
+print('Sigma X:\n', sx)
+print('Sigma Y:\n', sy)
+print('Sigma Z:\n', sz)
+
+
+# check equation:
+left_side = sx @ sy
+right_side = 1j * sz
+
+if np.allclose(left_side, right_side):
+	print('The equation sxsy = isz holds.')
+else:
+	print('The equation sxsy = isz does NOT hold.')
--- src/fp_qsim/pauli.py
+++ src/fp_qsim/pauli.py
@@ -11,22 +11,3 @@ def sigmay() -> np.ndarray:
 
 def sigmaz() -> np.ndarray:
 	return np.array([[1, 0], [0, -1]], dtype=complex)
-
-
-sx: np.ndarray = sigmax()
-sy: np.ndarray = sigmay()
-sz: np.ndarray = sigmaz()
-
-print('Sigma X:\n', sx)
-print('Sigma Y:\n', sy)
-print('Sigma Z:\n', sz)
-
-
-# check equation:
-left_side = sx @ sy
-right_side = 1j * sz
-
-if np.allclose(left_side, right_side):
-	print('The equation sxsy = isz holds.')
-else:
-	print('The equation sxsy = isz does NOT hold.')
