e3f028c926dbd51425b86ace6ae22d42545174d4
bennimaximi <benjamingrasselt@gmail.com>
2026-03-24 13:01:51 +0100

    minor adjustments

--- src/fp_qsim/simulator.py
+++ src/fp_qsim/simulator.py
@@ -1,16 +1,15 @@
 # mocked simulator for testing purposes
-from qiskit_aer import AerSimulator, StatevectorSimulator
+from qiskit_aer import AerSimulator
 from dataclasses import dataclass
+
 
 @dataclass
 class MockSimulator(AerSimulator):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._statevector_simulator = AerSimulator(method='statevector')
-        
-    
-    def run(self, circuits, *args, **kwargs):
-        # For testing purposes, we return a statevector instead of a measurement
-        result = self._statevector_simulator.run(circuits).result()
-        return result
+	def __init__(self, *args: object, **kwargs: object) -> None:
+		super().__init__(*args, **kwargs)
+		self._statevector_simulator = AerSimulator(method='statevector')
 
+	def run(self, circuits: object, *args: object, **kwargs: object) -> object:
+		# For testing purposes, we return a statevector instead of a measurement
+		result = self._statevector_simulator.run(circuits).result()
+		return result
