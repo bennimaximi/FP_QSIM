b9e597ee6bf99b1f3e37c633885a9a8ad00ec1b5
bennimaximi <benjamingrasselt@gmail.com>
2026-03-24 14:11:20 +0100

    implemented mock simulator and tested in ipynb file

--- src/fp_qsim/simulator.py
+++ src/fp_qsim/simulator.py
@@ -1,15 +1,15 @@
 # mocked simulator for testing purposes
+from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 from dataclasses import dataclass
 
 
 @dataclass
 class MockSimulator(AerSimulator):
-	def __init__(self, *args: object, **kwargs: object) -> None:
-		super().__init__(*args, **kwargs)
+	def __init__(self) -> None:
+		super().__init__()
 		self._statevector_simulator = AerSimulator(method='statevector')
 
-	def run(self, circuits: object, *args: object, **kwargs: object) -> object:
-		# For testing purposes, we return a statevector instead of a measurement
-		result = self._statevector_simulator.run(circuits).result()
+	def run(self, circuits: QuantumCircuit, shots: int) -> object:
+		result = self._statevector_simulator.run(circuits, shots=shots).result()
 		return result
