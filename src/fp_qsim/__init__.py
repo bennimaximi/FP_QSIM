a9d4bbf1d777fd6f1b0bc22f7f65cbbfd5ec586a
MaximilianHaug <st182808@stud.uni-stuttgart.de>
2026-03-27 12:00:10 +0100

    gpu added

--- src/fp_qsim/__init__.py
+++ src/fp_qsim/__init__.py
@@ -1,11 +1,13 @@
 """Public package exports for fp_qsim."""
 
 from fp_qsim.simulator import CustomSimulatorGeneral, CustomSimulatorManual, MockSimulator
+from fp_qsim.simulator_gpu import CustomSimulatorManualGPU
 from fp_qsim.simulator_optimized import CustomSimulatorManualOptimized
 from fp_qsim.state_vector import mocked_statevector
 
 __all__ = [
     "CustomSimulatorManual",
+    "CustomSimulatorManualGPU",
     "CustomSimulatorManualOptimized",
     "MockSimulator",
     "CustomSimulatorGeneral",
